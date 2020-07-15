46641461887715818418d9a90d414eacb2c27480
Victor Vilela Duarte <victorvduarte@gmail.com>
2020-07-15 00:53:56 -0300

    Migration para tabela de veiculos

--- backend/src/migrations/20200715004030_create_table_veiculos.js
+++ backend/src/migrations/20200715004030_create_table_veiculos.js
@@ -1,8 +1,14 @@
+exports.up = (knex) =>
+  knex.schema.createTable("veiculos", (table) => {
+    table.increments("id");
+    table.string("veiculo");
+    table.string("marca");
+    table.integer("ano");
+    table.text("descricao");
+    table.boolean("vendido");
 
-exports.up = function(knex) {
-  
-};
+    table.timestamp("created").defaultTo(knex.fn.now());
+    table.timestamp("updated").defaultTo(knex.fn.now());
+  });
 
-exports.down = function(knex) {
-  
-};
+exports.down = (knex) => knex.schema.dropTable("veiculos");
